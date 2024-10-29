81381f117701c425d3aa81816a5475079260c0e7
alexw <alex.j.witkowski@gmail.com>
2024-10-29 17:31:27 -0400

    Attempt import from figma

---
+++ src/routes/FilterColumn.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+</script>
+
+<aside class="flex flex-col w-[19%] max-md:ml-0 max-md:w-full">
+  <div class="flex flex-col grow pb-80 w-full text-xl whitespace-nowrap bg-white border border-gray-200 border-solid text-neutral-900 max-md:pb-24 max-md:mt-3.5">
+    <div class="flex relative flex-col px-16 pt-4 pb-7 aspect-[3.377] max-w-[205px] max-md:px-5">
+      <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/bb58a2937086542628bfcbb2bc91287558845307f4b4f4eda501b652441bb351?placeholderIfAbsent=true&apiKey=7d82fdb3593d4b2699db1ab59e93f211" alt="" class="object-cover absolute inset-0 size-full" />
+      <h2>Filters</h2>
+    </div>
+    <div class="flex relative flex-col px-12 pt-4 pb-7 mt-80 aspect-[3.377] max-w-[204px] max-md:px-5 max-md:mt-10">
+      <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/b96d1ad6a42334ef34e97e422ea939eb3c62e53fad0d028aa60f51b7a221e970?placeholderIfAbsent=true&apiKey=7d82fdb3593d4b2699db1ab59e93f211" alt="" class="object-cover absolute inset-0 size-full" />
+      <h2>Anti-Filters</h2>
+    </div>
+  </div>
+</aside>
---
+++ src/routes/JobBoard.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+</script>
+
+<section class="flex flex-col ml-5 w-[81%] max-md:ml-0 max-md:w-full">
+  <div class="flex flex-col grow items-start pr-20 w-full bg-white border border-gray-200 border-solid shadow-[0px_4px_4px_rgba(0,0,0,0.25)] max-md:mt-3.5 max-md:max-w-full">
+    <div class="flex shrink-0 max-w-full bg-sky-600 h-[59px] w-[385px]" role="banner" aria-label="Job board header"></div>
+    <div class="flex shrink-0 max-w-full bg-white border border-gray-200 border-solid h-[705px] w-[385px]" role="region" aria-label="Job listings"></div>
+  </div>
+</section>
