45cb7e33f13df7e2886972191a5d855774e8206d
alexw <alex.j.witkowski@gmail.com>
2024-10-29 19:16:43 -0400

    Attempt import from figma

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,5 @@
-<script lang="ts">
-import JobSearchHeader from './JobSearchHeader.svelte';
-import FilterColumn from './FilterColumn.svelte';
-import JobBoard from './JobBoard.svelte';
-
+<script>
+  import JobSearchComponent from '$lib/components/JobSearchComponent.svelte';
 </script>
 
-<main class="flex overflow-hidden flex-col bg-stone-100">
-  <JobSearchHeader />
-  <div class="self-center w-full max-w-[1074px] max-md:max-w-full">
-    <div class="flex gap-5 max-md:flex-col">
-      <FilterColumn />
-      <JobBoard />
-    </div>
-  </div>
-</main>
+<JobSearchComponent />
